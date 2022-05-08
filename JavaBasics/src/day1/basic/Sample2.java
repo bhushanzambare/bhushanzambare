4078b33cbe03c661ab4184a483436be708d984d1
bhushanzambare <bhushanzambare1993@gmail.com>
2022-05-08 16:16:35 +0530

    java Basics

---
+++ JavaBasics/src/day1/basic/Sample2.java
@@ -0,0 +1,17 @@
+package day1.basic;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi,Good Morning");
+		System.out.println("Welcome to Java class");
+		System.out.println("Iam Bhushan");
+		// or
+		System.out.println("Hi, Good Morning\nWelcome to Java Class\nI am Bhushan");
+		System.out.println("Iam from pune,Pincode=425102");
+//or
+		System.out.println("Iam from Pune,Pincode=\t425102");
+	}
+
+}
